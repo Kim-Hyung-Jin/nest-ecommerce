e167f508a3c9ae8dd69c9ba6649c60bb67550fa2
Hyungjin Kim <jeenz93@gmail.com>
2021-11-07 19:32:25 +0900

    merge feature/product-graphql

--- src/interfaces/graphql/products.resolver.ts
+++ src/interfaces/graphql/products.resolver.ts
@@ -9,11 +9,14 @@ import {
 import { Inject } from '@nestjs/common';
 import { ProductsService } from '../../domain/products.service';
 import { ProductsInfo } from '../../domain/dto/products.info';
+import { CreateProductDto } from '../dto/create-product.dto';
+import { ProductsDtoMapper } from '../products-dto.mapper';
 
 @Resolver('Products')
 export class ProductsResolver {
   constructor(
     @Inject('ProductsService') private productsService: ProductsService,
+    private productsDtoMapper: ProductsDtoMapper,
   ) {}
 
   @Query()
@@ -24,5 +27,9 @@ export class ProductsResolver {
   }
 
   @Mutation()
-  async register() {}
+  async registerProduct(@Args('data') dto: CreateProductDto) {
+    // TODO http request랑 같은거 써도 되나
+    const command = this.productsDtoMapper.toCreateProductCommand(dto);
+    return this.productsService.register(command);
+  }
 }
