0dd0889aaef3519d4d2d07bab8aeddce0e520c62
Hyungjin Kim <jeenz93@gmail.com>
2021-11-09 22:36:58 +0900

    service updateProduct error testcase

--- test/domain/products.service.spec.ts
+++ test/domain/products.service.spec.ts
@@ -8,8 +8,13 @@ import * as uuid from 'uuid';
 import { Products } from '../../src/domain/entity/product.entity';
 import ProductOptionGroup from '../../src/domain/entity/product-option-group.entity';
 import ProductOption from '../../src/domain/entity/product-option.entity';
-import { fixtureCreateCommand, fixtureProduct, fixtureUpdateProductCommand } from '../fixture';
+import {
+  fixtureCreateCommand,
+  fixtureProduct,
+  fixtureUpdateProductCommand,
+} from '../fixture';
 import { CreateProductCommand } from '../../src/domain/dto/create-product.command';
+import { EntityNotFoundError } from 'typeorm';
 
 jest.mock('uuid');
 
@@ -193,7 +198,7 @@ describe('updateProduct() 호출시', () => {
     service = module.get<ProductsService>('ProductsService');
   });
 
-  describe('올바른 productCode 가 주어지면', () => {
+  describe('올바른 command 가 주어지면', () => {
     function makeMockEntity(productCode) {
       const mockedEntity = fixtureProduct();
       Reflect.deleteProperty(mockedEntity, 'productCode');
@@ -240,6 +245,147 @@ describe('updateProduct() 호출시', () => {
         updatedProduct,
       );
       expect(res).toStrictEqual(expectedInfo);
+    });
+  });
+
+  describe('productPrice 만 빈값으로 주어지면', () => {
+    function makeMockEntity(productCode) {
+      const mockedEntity = fixtureProduct();
+      Reflect.deleteProperty(mockedEntity, 'productCode');
+      Reflect.deleteProperty(mockedEntity, 'status');
+      Reflect.set(mockedEntity, 'productCode', productCode);
+      Reflect.set(mockedEntity, 'status', '준비중');
+
+      return mockedEntity;
+    }
+
+    function makeExpectedUpdatedProduct(
+      mockedEntity,
+      command: { productCode: any; productName: any; productPrice: any },
+    ) {
+      const expectedUpdatedProduct = mockedEntity;
+      Reflect.deleteProperty(expectedUpdatedProduct, 'productName');
+      Reflect.deleteProperty(expectedUpdatedProduct, 'productPrice');
+      Reflect.set(expectedUpdatedProduct, 'productName', command.productName);
+      Reflect.set(expectedUpdatedProduct, 'productPrice', command.productPrice);
+      return expectedUpdatedProduct;
+    }
+
+    it('product 응답', async () => {
+      const productCode = faker.datatype.uuid();
+      const command = fixtureUpdateProductCommand(productCode);
+      delete command.productPrice;
+      const mockRetrievedProduct = makeMockEntity(productCode);
+      const mockStoredProduct = makeExpectedUpdatedProduct(
+        mockRetrievedProduct,
+        command,
+      );
+      const updatedProduct = mockStoredProduct;
+      const expectedInfo = { ...updatedProduct };
+
+      mockReader.getProductBy.mockReturnValue(mockRetrievedProduct);
+      mockStore.store.mockReturnValue(mockStoredProduct);
+      mockReader.getAllOptionInfoList.mockReturnValue(
+        mockRetrievedProduct.productOptionGroupList,
+      );
+
+      const res = await service.updateProduct(command);
+      expect(mockReader.getProductBy).toHaveBeenCalledWith(command.productCode);
+      expect(mockStore.store).toHaveBeenCalledWith(updatedProduct);
+      expect(mockReader.getAllOptionInfoList).toHaveBeenCalledWith(
+        updatedProduct,
+      );
+      expect(res).toStrictEqual(expectedInfo);
+    });
+  });
+
+  describe('productName 만 빈값으로 주어지면', () => {
+    function makeMockEntity(productCode) {
+      const mockedEntity = fixtureProduct();
+      Reflect.deleteProperty(mockedEntity, 'productCode');
+      Reflect.deleteProperty(mockedEntity, 'status');
+      Reflect.set(mockedEntity, 'productCode', productCode);
+      Reflect.set(mockedEntity, 'status', '준비중');
+
+      return mockedEntity;
+    }
+
+    function makeExpectedUpdatedProduct(
+      mockedEntity,
+      command: { productCode: any; productName: any; productPrice: any },
+    ) {
+      const expectedUpdatedProduct = mockedEntity;
+      Reflect.deleteProperty(expectedUpdatedProduct, 'productName');
+      Reflect.deleteProperty(expectedUpdatedProduct, 'productPrice');
+      Reflect.set(expectedUpdatedProduct, 'productName', command.productName);
+      Reflect.set(expectedUpdatedProduct, 'productPrice', command.productPrice);
+      return expectedUpdatedProduct;
+    }
+
+    it('product 응답', async () => {
+      const productCode = faker.datatype.uuid();
+      const command = fixtureUpdateProductCommand(productCode);
+      delete command.productName;
+      const mockRetrievedProduct = makeMockEntity(productCode);
+      const mockStoredProduct = makeExpectedUpdatedProduct(
+        mockRetrievedProduct,
+        command,
+      );
+      const updatedProduct = mockStoredProduct;
+      const expectedInfo = { ...updatedProduct };
+
+      mockReader.getProductBy.mockReturnValue(mockRetrievedProduct);
+      mockStore.store.mockReturnValue(mockStoredProduct);
+      mockReader.getAllOptionInfoList.mockReturnValue(
+        mockRetrievedProduct.productOptionGroupList,
+      );
+
+      const res = await service.updateProduct(command);
+      expect(mockReader.getProductBy).toHaveBeenCalledWith(command.productCode);
+      expect(mockStore.store).toHaveBeenCalledWith(updatedProduct);
+      expect(mockReader.getAllOptionInfoList).toHaveBeenCalledWith(
+        updatedProduct,
+      );
+      expect(res).toStrictEqual(expectedInfo);
+    });
+  });
+
+  describe('productName, productPrice 모두 빈값으로 주어지면', () => {
+    function makeMockEntity(productCode) {
+      const mockedEntity = fixtureProduct();
+      Reflect.deleteProperty(mockedEntity, 'productCode');
+      Reflect.deleteProperty(mockedEntity, 'status');
+      Reflect.set(mockedEntity, 'productCode', productCode);
+      Reflect.set(mockedEntity, 'status', '준비중');
+
+      return mockedEntity;
+    }
+
+    function makeExpectedUpdatedProduct(
+      mockedEntity,
+      command: { productCode: any; productName: any; productPrice: any },
+    ) {
+      const expectedUpdatedProduct = mockedEntity;
+      Reflect.deleteProperty(expectedUpdatedProduct, 'productName');
+      Reflect.deleteProperty(expectedUpdatedProduct, 'productPrice');
+      Reflect.set(expectedUpdatedProduct, 'productName', command.productName);
+      Reflect.set(expectedUpdatedProduct, 'productPrice', command.productPrice);
+      return expectedUpdatedProduct;
+    }
+
+    it('product 응답', async () => {
+      const productCode = faker.datatype.uuid();
+      const command = fixtureUpdateProductCommand(productCode);
+      delete command.productName;
+      delete command.productPrice;
+      const mockRetrievedProduct = makeMockEntity(productCode);
+      mockReader.getProductBy.mockReturnValue(mockRetrievedProduct);
+
+      // TODO async 인 이유 확인
+      await expect(async () => {
+        await service.updateProduct(command);
+      }).rejects.toThrowError(new Error('업데이트 할 값이 없음'));
+      expect(mockReader.getProductBy).toHaveBeenCalledWith(command.productCode);
     });
   });
 });
